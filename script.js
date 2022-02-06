9503a4a76b3260fda49e3bd5aba189b4bef9bb82
unknown <buchimmaduabuchukwu@gmail.com>
2022-02-06 19:05:58 +0100

    section-3

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu=document.querySelector('.menu')
 const wrapper=document.querySelectorAll('.wrapper')
+const video=document.querySelectorAll('.video')
 
 menu.addEventListener('click',()=>{
     document.querySelectorAll('.target').forEach((btn)=>{
@@ -11,5 +12,13 @@ wrapper.forEach((press)=>{
         document.querySelectorAll('.target').forEach((close)=>{
             close.classList.remove('change')
         })
+    })
+})
+video.forEach((vid)=>{
+    vid.addEventListener('mouseover',()=>{
+        vid.play()
+    })
+    vid.addEventListener('mouseout',()=>{
+        vid.pause()
     })
 })
